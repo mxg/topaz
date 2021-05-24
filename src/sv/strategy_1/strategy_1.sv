e4ca0cb7d7e560c53f7fe788a99ff47a0439d28c
Mark Glasser <mxg@tsoft.com>
2021-05-23 18:23:47 -0700

    Improvements to strategy_1

--- src/sv/strategy_1/strategy_1.sv
+++ src/sv/strategy_1/strategy_1.sv
@@ -1,16 +1,19 @@
 virtual class strategy;
+  pure virtual function void operation();
 endclass
 
 class concrete_strategy_1 extends strategy;
+  virtual function void operation();
+  endfunction
 endclass
 
 class concrete_strategy_2 extends strategy;
+  virtual function void operation();
+  endfunction
 endclass
 
 class some_context;
-
   strategy stgy;
-
 endclass
 
 module top;
