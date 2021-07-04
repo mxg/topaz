98c638c83b2717c36700609ace5c442fe54ed0bc
Mark Glasser <mxg@tsoft.com>
2021-07-04 13:21:33 -0700

    A few tweaks

--- src/sv/strategy/cc_fsm_base.svh
+++ src/sv/strategy/cc_fsm_base.svh
@@ -5,15 +5,11 @@ virtual class cc_fsm_base extends uvm_object;
 
   `uvm_object_abstract_utils(cc_fsm_base)
 
-  // A row is an associative array of states indexed by operation.
   typedef state_t row_t[operation_t];
-  // A state table is an associative array of rows indexed by state.
   typedef row_t table_t[state_t];
 
-  // Table of state transitions
   protected table_t state_table;
 
-  // table of current states by address
   protected state_t state[addr_t];
 
   function new(string name = "cc_fsm_base");
@@ -29,46 +25,41 @@ virtual class cc_fsm_base extends uvm_object;
   // Operate the state machine to update the cache state for a
   // particular address
   //----------------------------------------------------------------------------
-  virtual function void update_state(operation_t op, addr_t addr);
+  virtual function void update_state(operation_t op,
+				     addr_t addr);
     state_t curr_state;
     row_t curr_row;
     state_t next_state;
 
-    // Get the current state for the address
     if(state.exists(addr))
       curr_state = state[addr];
     else
       curr_state = state_S;
 
-    // Is the current state a valid state? (This should always
-    // succeed.  If it does not something has gone terribly wrong,
-    // thus the call to uvm_fatal)
     if(state_table.exists(curr_state))
-      begin
 	curr_row = state_table[curr_state];
-      end
     else
-      begin
-	`uvm_fatal("CC_FSM", $sformatf("no state table entry for state %s",
-				       curr_state.name()))
-      end
+	`uvm_fatal("CC_FSM", 
+		   ("no state table entry for state %s",
+		    curr_state.name()))
 
-    // If we have a valid state transition update the state for the
-    // address, otherwise issue an error and attempt to resume at some
-    // rational state.
     if(curr_row.exists(op)) begin
       next_state = curr_row[op];
       state[addr] = next_state;
-      `uvm_info("CC_FSM", $sformatf("%16x : %s(%s) --> %s",
-				    addr, curr_state.name(), 
-				    op.name(), next_state.name()),
+      `uvm_info("CC_FSM",
+		$sformatf("%16x : %s(%s) --> %s",
+			  addr, curr_state.name(), 
+			  op.name(), next_state.name()),
 		UVM_NONE)
     end
     else begin
-      `uvm_error("CC_FSM", $sformatf("invalid state transition from %s with operation %s at addr \'h%0x",
-				     curr_state.name(), op.name(), addr))
-      // assume the contents of the memory location are now invalid
-      state[addr] = state_I;
+      `uvm_error("CC_FSM",
+		 $sformatf({"invalid state transition ",
+                            "from %s with operation %s",
+			    "at addr \'h%0x"},
+			   curr_state.name(),
+			   op.name(), addr))
+       state[addr] = state_I;
     end
     
   endfunction
--- src/uvm/regex_toy/regex_toy.sv
+++ src/uvm/regex_toy/regex_toy.sv
@@ -1,18 +1,13 @@
 import uvm_pkg::*;
-
 class regex_toy;
-
-  static function void re_matcher(string regex, ref string strings[$]);
-    int unsigned i;
-    bit 	 match;
-
+  static function void re_matcher(string regex,
+                                  ref string strings[$]);
     $display("matches for regular expression : %s", regex);
     foreach(strings[i]) begin
-      match = uvm_is_match(regex, strings[i]);
+      bit match = uvm_is_match(regex, strings[i]);
       $display("%s : %s", (match?"match":"     "), strings[i]);
     end
   endfunction
-
 endclass
 
 module top;
