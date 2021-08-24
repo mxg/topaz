7981cced257f16cd05dab95166c584a802ae17d3
Mark Glasser <mark.glasser@cerebras.net>
2021-08-23 20:55:08 -0700

    A little fifo cleanup

--- src/uvm/fifo/env.svh
+++ src/uvm/fifo/env.svh
@@ -13,6 +13,20 @@
 //
 //    TOPAZ is a library of SystemVerilog and UVM patterns and idioms.  The
 //    code is suitable for study and for copying/pasting into your own work.
+//
+//    Copyright 2021 Mark Glasser
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
 //------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------
@@ -45,7 +59,7 @@ class env extends uvm_component;
   endfunction
 
   //--------------------------------------------------------------------
-  // Connect things together.  COnnect the driver to the sequencer an
+  // Connect things together.  Connect the driver to the sequencer and
   // the talker to the monitor.
   // --------------------------------------------------------------------
   function void connect_phase(uvm_phase phase);
@@ -60,7 +74,7 @@ class env extends uvm_component;
   task run_phase(uvm_phase phase);
     phase.raise_objection(this);
     seq.start(sqr);
-    $display("drop objection");
+    phase.drop_objection(this);
   endtask
   
 endclass
