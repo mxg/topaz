f08733f3b23710d407b655996dfde8b60ace0e8c
Mark Glasser <mark.glasser@cerebras.net>
2021-09-02 21:06:26 -0700

    Update more file headers.

--- src/uvm/test/test.sv
+++ src/uvm/test/test.sv
@@ -13,6 +13,20 @@
 //
 //    TOPAZ is a library of SystemVerilog and UVM patterns and idioms.  The
 //    code is suitable for study and for copying/pasting into your own work.
+//
+//    Copyright 2021 Mark Glasser
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
@@ -25,7 +39,7 @@
 //------------------------------------------------------------------------------
 
 `include "uvm_macros.svh"
-import uvm_pkg::*
+import uvm_pkg::*;
 
 //------------------------------------------------------------------------------
 // component
@@ -87,7 +101,7 @@ module top;
 
   initial begin
     component c;
-    run_test()
+    run_test();
   end
 endmodule
 
