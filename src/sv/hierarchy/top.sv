c6c6d12db4a4bc5b213de8014ff097db48e09f67
Mark Glasser <markglasser@comcast.net>
2024-05-26 11:01:16 -0600

    removed extra blank lines in hierarchy/top

--- src/sv/hierarchy/top.sv
+++ src/sv/hierarchy/top.sv
@@ -45,14 +45,11 @@ module top;
     a = new("A", null); // topmost node has no parent
     b = new("B", a);
     c = new("C", a);
-
     d = new("D", b);
     e = new("E", b);
     f = new("F", b);
-
     g = new("G", c);
     h = new("H", c);
-     
     i = new("I", g);
   end
 
